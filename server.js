d47be0f5e7be14fc4d528027057a229e30e49307
Eric Tarter <etarter@ccli.com>
2023-07-26 13:36:09 -0700

    mongoose db added

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3004
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/counterDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const counterSchema = new mongoose.Schema({
+  name: String,
+  count: Number
+})
+
+const Counter = mongoose.model('Counter', counterSchema)
 
 let vueCounter = 0
 let reactCounter = 0
@@ -14,58 +29,92 @@ app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
+// Helper function to update the counter in the database
+const updateCounter = async (counterName, value) => {
+  try {
+    const counter = await Counter.findOne({ name: counterName })
+    if (counter) {
+      counter.count = value
+      await counter.save()
+    } else {
+      await Counter.create({ name: counterName, count: value })
+    }
+  } catch (err) {
+    console.error('Error updating counter:', err)
+  }
+}
+
 // Route for incrementing the counter
-app.get('/vuecounter/increment', (req, res) => {
+app.get('/vuecounter/increment', async (req, res) => {
   vueCounter++
+  await updateCounter('vueCounter', vueCounter)
   res.json({ vueCounter })
 })
 
-app.get('/reactcounter/increment', (req, res) => {
+app.get('/reactcounter/increment', async (req, res) => {
   reactCounter++
+  await updateCounter('reactCounter', reactCounter)
   res.json({ reactCounter })
 })
 
-app.get('/angularcounter/increment', (req, res) => {
+app.get('/angularcounter/increment', async (req, res) => {
   angularCounter++
+  await updateCounter('angularCounter', angularCounter)
   res.json({ angularCounter })
 })
 
-app.get('/othercounter/increment', (req, res) => {
+app.get('/othercounter/increment', async (req, res) => {
   otherCounter++
+  await updateCounter('otherCounter', otherCounter)
   res.json({ otherCounter })
 })
 
 // Route for decrementing the counter
-app.get('/vuecounter/decrement', (req, res) => {
+app.get('/vuecounter/decrement', async (req, res) => {
   if (vueCounter > 0) {
     vueCounter--
+    await updateCounter('vueCounter', vueCounter)
   }
   res.json({ vueCounter })
 })
 
-app.get('/reactcounter/decrement', (req, res) => {
+app.get('/reactcounter/decrement', async (req, res) => {
   if (reactCounter > 0) {
     reactCounter--
+    await updateCounter('reactCounter', reactCounter)
   }
   res.json({ reactCounter })
 })
 
-app.get('/angularcounter/decrement', (req, res) => {
+app.get('/angularcounter/decrement', async (req, res) => {
   if (angularCounter > 0) {
     angularCounter--
+    await updateCounter('angularCounter', angularCounter)
   }
   res.json({ angularCounter })
 })
 
-app.get('/othercounter/decrement', (req, res) => {
+app.get('/othercounter/decrement', async (req, res) => {
   if (otherCounter > 0) {
     otherCounter--
+    await updateCounter('otherCounter', otherCounter)
   }
   res.json({ otherCounter })
 })
 
-app.get('/count/all', (req, res) => {
-  res.json({ vueCounter, reactCounter, angularCounter, otherCounter })
+// Route to get all counters
+app.get('/count/all', async (req, res) => {
+  try {
+    const counters = await Counter.find()
+    const countersObject = counters.reduce((acc, counter) => {
+      acc[counter.name] = counter.count
+      return acc
+    }, {})
+    res.json(countersObject)
+  } catch (err) {
+    console.error('Error fetching counters:', err)
+    res.status(500).json({ error: 'Error fetching counters' })
+  }
 })
 
 app.get('/set-cookie', (req, res) => {
