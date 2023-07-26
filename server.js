97d2d205303d7a9d622cdf0f7922d5c8deb61f91
Eric Tarter <etarter@ccli.com>
2023-07-26 15:13:52 -0700

    back to default

--- server.js
+++ server.js
@@ -4,74 +4,78 @@ const cors = require('cors')
 const app = express()
 const port = 3004
 
-// In-memory database to store counter values
-let counters = {
-  vueCounter: 0,
-  reactCounter: 0,
-  angularCounter: 0,
-  otherCounter: 0
-}
+let vueCounter = 0
+let reactCounter = 0
+let angularCounter = 0
+let otherCounter = 0
 
 // Middleware to parse JSON bodies
 app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
-// Function to fetch all counters from the database
-function getCounters(res) {
-  res.json(counters)
-}
-
-// Function to update a counter value in the database
-function updateCounter(counterName, newValue, res) {
-  counters[counterName] = newValue
-  getCounters(res)
-}
-
 // Route for incrementing the counter
 app.get('/vuecounter/increment', (req, res) => {
-  updateCounter('vueCounter', counters.vueCounter + 1, res)
+  vueCounter++
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/increment', (req, res) => {
-  updateCounter('reactCounter', counters.reactCounter + 1, res)
+  reactCounter++
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/increment', (req, res) => {
-  updateCounter('angularCounter', counters.angularCounter + 1, res)
+  angularCounter++
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/increment', (req, res) => {
-  updateCounter('otherCounter', counters.otherCounter + 1, res)
+  otherCounter++
+  res.json({ otherCounter })
 })
 
 // Route for decrementing the counter
 app.get('/vuecounter/decrement', (req, res) => {
-  const newCounterValue = Math.max(0, counters.vueCounter - 1)
-  updateCounter('vueCounter', newCounterValue, res)
+  if (vueCounter > 0) {
+    vueCounter--
+  }
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/decrement', (req, res) => {
-  const newCounterValue = Math.max(0, counters.reactCounter - 1)
-  updateCounter('reactCounter', newCounterValue, res)
+  if (reactCounter > 0) {
+    reactCounter--
+  }
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/decrement', (req, res) => {
-  const newCounterValue = Math.max(0, counters.angularCounter - 1)
-  updateCounter('angularCounter', newCounterValue, res)
+  if (angularCounter > 0) {
+    angularCounter--
+  }
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/decrement', (req, res) => {
-  const newCounterValue = Math.max(0, counters.otherCounter - 1)
-  updateCounter('otherCounter', newCounterValue, res)
+  if (otherCounter > 0) {
+    otherCounter--
+  }
+  res.json({ otherCounter })
 })
 
-// Route to get all counters
 app.get('/count/all', (req, res) => {
-  getCounters(res)
+  res.json({ vueCounter, reactCounter, angularCounter, otherCounter })
 })
 
-// Root URL route
+app.get('/set-cookie', (req, res) => {
+  res.cookie('cookieName', 'cookieValue', {
+    sameSite: 'none',
+    secure: true
+  })
+  res.send('Cookie set successfully!')
+})
+
 app.get('/', (req, res) => {
   res.send('Hello from Eric!')
 })
