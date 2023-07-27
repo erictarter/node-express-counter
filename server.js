6949e9244fd4664c3382504d524411ed9d50a9e7
Eric Tarter <etarter@ccli.com>
2023-07-27 13:26:06 -0700

    back to working but not saving dating when it restarts

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const cors = require('cors')
 const app = express()
 const port = 3004
 
-// Require the SQLite database connection
-const db = require('./database')
-
 let vueCounter = 0
 let reactCounter = 0
 let angularCounter = 0
@@ -19,248 +16,56 @@ app.use(express.json())
 
 // Route for incrementing the counter
 app.get('/vuecounter/increment', (req, res) => {
-  db.run(
-    'UPDATE counters SET vueCounter = vueCounter + 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Failed to increment vueCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT vueCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-        vueCounter = row.vueCounter
-        res.json({ vueCounter })
-      })
-    }
-  )
+  vueCounter++
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/increment', (req, res) => {
-  db.run(
-    'UPDATE counters SET reactCounter = reactCounter + 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to increment reactCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT reactCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-        reactCounter = row.reactCounter
-        res.json({ reactCounter })
-      })
-    }
-  )
+  reactCounter++
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/increment', (req, res) => {
-  db.run(
-    'UPDATE counters SET angularCounter = angularCounter + 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to increment angularCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT angularCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-        angularCounter = row.angularCounter
-        res.json({ angularCounter })
-      })
-    }
-  )
+  angularCounter++
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/increment', (req, res) => {
-  db.run(
-    'UPDATE counters SET otherCounter = otherCounter + 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to increment otherCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT otherCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-        otherCounter = row.otherCounter
-        res.json({ otherCounter })
-      })
-    }
-  )
+  otherCounter++
+  res.json({ otherCounter })
 })
 
 // Route for decrementing the counter
 app.get('/vuecounter/decrement', (req, res) => {
-  db.run(
-    'UPDATE counters SET vueCounter = vueCounter - 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Failed to decrement vueCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT vueCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-
-        // Update the counter in memory as well
-        if (vueCounter > 0) {
-          vueCounter--
-        }
-
-        res.json({ vueCounter })
-      })
-    }
-  )
+  if (vueCounter > 0) {
+    vueCounter--
+  }
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/decrement', (req, res) => {
-  db.run(
-    'UPDATE counters SET reactCounter = reactCounter - 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to decrement reactCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT reactCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-
-        // Update the counter in memory as well
-        if (reactCounter > 0) {
-          reactCounter--
-        }
-
-        res.json({ reactCounter })
-      })
-    }
-  )
+  if (reactCounter > 0) {
+    reactCounter--
+  }
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/decrement', (req, res) => {
-  db.run(
-    'UPDATE counters SET angularCounter = angularCounter - 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to decrement angularCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT angularCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-
-        // Update the counter in memory as well
-        if (angularCounter > 0) {
-          angularCounter--
-        }
-
-        res.json({ angularCounter })
-      })
-    }
-  )
+  if (angularCounter > 0) {
+    angularCounter--
+  }
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/decrement', (req, res) => {
-  db.run(
-    'UPDATE counters SET otherCounter = otherCounter - 1 WHERE id = 1',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return res
-          .status(500)
-          .json({ error: 'Failed to decrement otherCounter' })
-      }
-
-      // Fetch the updated value from the database and send it in the response
-      db.get('SELECT otherCounter FROM counters WHERE id = 1', (err, row) => {
-        if (err) {
-          console.error(err)
-          return res
-            .status(500)
-            .json({ error: 'Failed to fetch counter value' })
-        }
-
-        // Update the counter in memory as well
-        if (otherCounter > 0) {
-          otherCounter--
-        }
-
-        res.json({ otherCounter })
-      })
-    }
-  )
+  if (otherCounter > 0) {
+    otherCounter--
+  }
+  res.json({ otherCounter })
 })
 
 app.get('/count/all', (req, res) => {
-  db.get(
-    'SELECT vueCounter, reactCounter, angularCounter, otherCounter FROM counters WHERE id = 1',
-    (err, row) => {
-      if (err) {
-        console.error(err)
-        return res.status(500).json({ error: 'Failed to fetch counters' })
-      }
-
-      // Update the counters in memory as well
-      vueCounter = row.vueCounter
-      reactCounter = row.reactCounter
-      angularCounter = row.angularCounter
-      otherCounter = row.otherCounter
-
-      res.json(row)
-    }
-  )
+  res.json({ vueCounter, reactCounter, angularCounter, otherCounter })
 })
 
 app.get('/set-cookie', (req, res) => {
