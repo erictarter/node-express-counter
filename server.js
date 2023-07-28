11c0487bd95e1f51e7c4fe8d827cf601066c128a
Eric Tarter <etarter@ccli.com>
2023-07-28 08:04:43 -0700

    back to no db

--- server.js
+++ server.js
@@ -1,103 +1,71 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const Database = require('better-sqlite3')
-
 const app = express()
 const port = 3004
 
-const db = new Database('counters.db', {})
+let vueCounter = 0
+let reactCounter = 0
+let angularCounter = 0
+let otherCounter = 0
 
-const incrementCounter = (counterName, callback) => {
-  const stmt = db.prepare('SELECT value FROM counters WHERE name = ?')
-  const row = stmt.get(counterName)
-  let value = row ? row.value : 0
-  value++
-
-  const updateStmt = db.prepare(
-    'INSERT OR REPLACE INTO counters (name, value) VALUES (?, ?)'
-  )
-  updateStmt.run(counterName, value)
-
-  callback(value)
-}
-
-const decrementCounter = (counterName, callback) => {
-  const stmt = db.prepare('SELECT value FROM counters WHERE name = ?')
-  const row = stmt.get(counterName)
-  let value = row ? row.value : 0
-  if (value > 0) {
-    value--
-  }
-
-  const updateStmt = db.prepare(
-    'INSERT OR REPLACE INTO counters (name, value) VALUES (?, ?)'
-  )
-  updateStmt.run(counterName, value)
-
-  callback(value)
-}
-
+// Middleware to parse JSON bodies
 app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
+// Route for incrementing the counter
 app.get('/vuecounter/increment', (req, res) => {
-  incrementCounter('vueCounter', (value) => {
-    res.json({ vueCounter: value })
-  })
+  vueCounter++
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/increment', (req, res) => {
-  incrementCounter('reactCounter', (value) => {
-    res.json({ reactCounter: value })
-  })
+  reactCounter++
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/increment', (req, res) => {
-  incrementCounter('angularCounter', (value) => {
-    res.json({ angularCounter: value })
-  })
+  angularCounter++
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/increment', (req, res) => {
-  incrementCounter('otherCounter', (value) => {
-    res.json({ otherCounter: value })
-  })
+  otherCounter++
+  res.json({ otherCounter })
 })
 
+// Route for decrementing the counter
 app.get('/vuecounter/decrement', (req, res) => {
-  decrementCounter('vueCounter', (value) => {
-    res.json({ vueCounter: value })
-  })
+  if (vueCounter > 0) {
+    vueCounter--
+  }
+  res.json({ vueCounter })
 })
 
 app.get('/reactcounter/decrement', (req, res) => {
-  decrementCounter('reactCounter', (value) => {
-    res.json({ reactCounter: value })
-  })
+  if (reactCounter > 0) {
+    reactCounter--
+  }
+  res.json({ reactCounter })
 })
 
 app.get('/angularcounter/decrement', (req, res) => {
-  decrementCounter('angularCounter', (value) => {
-    res.json({ angularCounter: value })
-  })
+  if (angularCounter > 0) {
+    angularCounter--
+  }
+  res.json({ angularCounter })
 })
 
 app.get('/othercounter/decrement', (req, res) => {
-  decrementCounter('otherCounter', (value) => {
-    res.json({ otherCounter: value })
-  })
+  if (otherCounter > 0) {
+    otherCounter--
+  }
+  res.json({ otherCounter })
 })
 
 app.get('/count/all', (req, res) => {
-  const stmt = db.prepare('SELECT name, value FROM counters')
-  const rows = stmt.all()
-
-  const counters = {}
-  rows.forEach((row) => (counters[row.name] = row.value))
-
-  res.json(counters)
+  res.json({ vueCounter, reactCounter, angularCounter, otherCounter })
 })
 
 app.get('/set-cookie', (req, res) => {
