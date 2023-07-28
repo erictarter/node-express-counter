ef8976d6d1b97f4b11e84aaa928996256c5b0e53
Eric Tarter <etarter@ccli.com>
2023-07-28 09:24:46 -0700

    attempting with file storage

--- server.js
+++ server.js
@@ -1,71 +1,97 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const fs = require('fs')
 const app = express()
 const port = 3004
 
-let vueCounter = 0
-let reactCounter = 0
-let angularCounter = 0
-let otherCounter = 0
+let counterData = {
+  vueCounter: 0,
+  reactCounter: 0,
+  angularCounter: 0,
+  otherCounter: 0
+}
 
-// Middleware to parse JSON bodies
+const saveCounterDataToFile = () => {
+  fs.writeFile('counterData.json', JSON.stringify(counterData), (err) => {
+    if (err) {
+      console.error('Error saving counter data:', err)
+    }
+  })
+}
+
+fs.readFile('counterData.json', (err, data) => {
+  if (!err) {
+    try {
+      counterData = JSON.parse(data)
+    } catch (e) {
+      console.error('Error parsing counter data:', e)
+    }
+  }
+})
+
 app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
-// Route for incrementing the counter
 app.get('/vuecounter/increment', (req, res) => {
-  vueCounter++
-  res.json({ vueCounter })
+  counterData.vueCounter++
+  saveCounterDataToFile()
+  res.json({ vueCounter: counterData.vueCounter })
 })
 
 app.get('/reactcounter/increment', (req, res) => {
-  reactCounter++
-  res.json({ reactCounter })
+  counterData.reactCounter++
+  saveCounterDataToFile()
+  res.json({ reactCounter: counterData.reactCounter })
 })
 
 app.get('/angularcounter/increment', (req, res) => {
-  angularCounter++
-  res.json({ angularCounter })
+  counterData.angularCounter++
+  saveCounterDataToFile()
+  res.json({ angularCounter: counterData.angularCounter })
 })
 
 app.get('/othercounter/increment', (req, res) => {
-  otherCounter++
-  res.json({ otherCounter })
+  counterData.otherCounter++
+  saveCounterDataToFile()
+  res.json({ otherCounter: counterData.otherCounter })
 })
 
-// Route for decrementing the counter
 app.get('/vuecounter/decrement', (req, res) => {
-  if (vueCounter > 0) {
-    vueCounter--
+  if (counterData.vueCounter > 0) {
+    counterData.vueCounter--
+    saveCounterDataToFile()
   }
-  res.json({ vueCounter })
+  res.json({ vueCounter: counterData.vueCounter })
 })
 
 app.get('/reactcounter/decrement', (req, res) => {
-  if (reactCounter > 0) {
-    reactCounter--
+  if (counterData.reactCounter > 0) {
+    counterData.reactCounter--
+    saveCounterDataToFile()
   }
-  res.json({ reactCounter })
+  res.json({ reactCounter: counterData.reactCounter })
 })
 
 app.get('/angularcounter/decrement', (req, res) => {
-  if (angularCounter > 0) {
-    angularCounter--
+  if (counterData.angularCounter > 0) {
+    counterData.angularCounter--
+    saveCounterDataToFile()
   }
-  res.json({ angularCounter })
+  res.json({ angularCounter: counterData.angularCounter })
 })
 
 app.get('/othercounter/decrement', (req, res) => {
-  if (otherCounter > 0) {
-    otherCounter--
+  if (counterData.otherCounter > 0) {
+    counterData.otherCounter--
+    saveCounterDataToFile()
   }
-  res.json({ otherCounter })
+  res.json({ otherCounter: counterData.otherCounter })
 })
 
 app.get('/count/all', (req, res) => {
-  res.json({ vueCounter, reactCounter, angularCounter, otherCounter })
+  res.json(counterData)
 })
 
 app.get('/set-cookie', (req, res) => {
@@ -80,7 +106,6 @@ app.get('/', (req, res) => {
   res.send('Hello from Eric!')
 })
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
